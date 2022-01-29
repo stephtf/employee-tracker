9c6997b8aeedd7ec37e158df3b36af3992ae0f9f
Stephanie Fajardo <stfajardo@gmail.com>
2022-01-29 16:12:35 -0500

    added seed data to the database

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Steph", "Fajardo", 1, 5),
+       ("Juba", "Diani", 2, 4),
+       ("Quiping", "Yu", 3, 3),
+       ("Manasa", "Gopal", 4, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (001, "Salesperson", 50000, 1),
+       (002, "Lead Engineer", 100000, 2),
+       (003, "Account Manager", 120000, 3),
+       (004, "Lawyer", 300000, 4);
+
+INSERT INTO department (id, department_name)
+VALUES (001, "Sales"),
+       (002, "Engineering"),
+       (003, "Finance"),
+       (004, "Legal");
