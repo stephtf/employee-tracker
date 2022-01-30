5c3d836f6ad85ca178d995c2548fc6b517971526
Stephanie Fajardo <stfajardo@gmail.com>
2022-01-30 14:15:15 -0500

    added queries for department and role

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,9 +6,9 @@ VALUES ("Steph", "Fajardo", 1, 5),
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (001, "Salesperson", 50000, 1),
-       (002, "Lead Engineer", 100000, 2),
-       (003, "Account Manager", 120000, 3),
-       (004, "Lawyer", 300000, 4);
+       (003, "Lead Engineer", 100000, 2),
+       (004, "Account Manager", 120000, 3),
+       (005, "Lawyer", 300000, 4);
 
 INSERT INTO department (id, department_name)
 VALUES (001, "Sales"),
--- server.js
+++ server.js
@@ -21,12 +21,25 @@ const company_db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
-// variables to later add value to database
-// const newDepartment = "Research"
-// const params = [`Research`];
+// variables for DEPARTMENT 
+// const newDepartment = `"${departmentInput}""`;
 
-// queries to add values into the database
-// company_db.query(`INSERT INTO department( department_name) VALUES (?)`, params, (err,result) => {
+// query for DEPARTMENT 
+// company_db.query(`INSERT INTO department( department_name) VALUES (newDepartment)`, (err,result) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.log(result)
+// });
+
+// variables for ROLE 
+// const newTitle = `"Sales Intern"`;
+// const newSalary = `"35000"`;
+// const newDepartmentId = `"1"`;
+
+
+// query for ROLE
+// company_db.query(`INSERT INTO role (title, salary, department_id) VALUES (${newTitle}, ${newSalary}, ${newDepartmentId})`, (err, result) => {
 //   if (err) {
 //     console.log(err)
 //   }
@@ -34,22 +47,20 @@ const company_db = mysql.createConnection(
 // });
 
 
+// variables for EMPLOYEE 
+const newFirst = `"Rafael"`; 
+const newLast = `"Nadal"`; 
+const newRoleId = `"6"`; 
+const newManagerId = `"7"`;
 
-// company_db.query(`INSERT INTO role (title, salary, department_id) VALUES (${newTitle}, ${newSalary}, ${newDepartmentId})`,  
-// (err,result) => {
-//   if (err) {
-//     console.log(err)
-//   }
-//   console.log(result)
-// });
-
-// company_db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${newFirst}, ${newLast}, ${newRoleId}, ${newManagerId})`,  
-// (err,result) => {
-//   if (err) {
-//     console.log(err)
-//   }
-//   console.log(result)
-// });
+// query for EMPLOYEE
+company_db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${newFirst}, ${newLast}, ${newRoleId}, ${newManagerId})`,  
+(err,result) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(result)
+});
 
 // // default response for any other request (Not Found)
 // app.use((req, res) => {
